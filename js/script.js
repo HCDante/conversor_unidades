36f0b3c59345eedbb936f51755d8d0abf562cb4a
hcdante <hectorhugoandradelinares@gmail.com>
2023-07-24 14:57:41 -0600

    Se agrega el conversor de unidades.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
-function convert() {
-    const inputUnits = document.getElementById('inputUnits').value;
-    const outputUnits = document.getElementById('outputUnits').value;
-    const inputValue = parseFloat(document.getElementById('inputValue').value);
+function validar() {
+  const valor = parseFloat(document.getElementById("valor").value);
+  const unidadOne = document.getElementById("unidadOne").value;
+  const unidadTwo = document.getElementById("unidadTwo").value;
 
-    let result = 0;
+  const conversion = {
+      mm: 0.1,
+      cm: 1,
+      in: 2.54,
+      ft: 30.48,
+      m: 100,
+      yd: 91.44,
+      km: 1000,
+      mi: 160934
+  };
 
-    if (inputUnits === 'mm') {
-      if (outputUnits === 'cm') {
-        result = inputValue / 10;
-      } else if (outputUnits === 'm') {
-        result = inputValue / 1000;
-      } else if (outputUnits === 'km') {
-        result = inputValue / 1000000;
-      } else if (outputUnits === 'in') {
-        result = inputValue / 25.4;
-      } else if (outputUnits === 'ft') {
-        result = inputValue / 304.8;
-      } else if (outputUnits === 'yd') {
-        result = inputValue / 914.4;
-      } else if (outputUnits === 'mi') {
-        result = inputValue / 1.609e+6;
-      } else {
-        result = inputValue;
-      }
-    } else if (inputUnits === 'cm') {
-      
-    }
+  const resultado = valor * conversion[unidadOne] / conversion[unidadTwo];
+  
+  document.getElementById("resultado").innerHTML = `Tu resultado es: ${resultado.toFixed(2)} ${unidadTwo}`
 
-    document.getElementById('result').innerText = `El resultado es: ${result.toFixed(2)} ${outputUnits}`;
-  }
+}
